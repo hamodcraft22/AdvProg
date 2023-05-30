3352b96f2ada964eeb74290e4567c09158a39b8a
hamodcraft22 <67144555+hamodcraft22@users.noreply.github.com>
2023-05-30 23:48:49 +0300

    updated files

--- advProj_BusinessObjects/AdvProg_ApplicationUser.cs
+++ advProj_BusinessObjects/AdvProg_ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace advProj_BusinessObjects
 {
     public class AdvProg_ApplicationUser : IdentityUser
     {
-
+        [Required]
+        public string? Name { get; set; }
+        public string? Department { get; set; }
+        public int? userID { get; set; }
     }
 }
